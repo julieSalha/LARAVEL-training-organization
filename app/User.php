ad078a56d0038d4a33b6a1237d7290c2b2347af4
Guilhem Fouassier <guilhem.fouassier@gmail.com>
2020-05-17 11:41:12 +0200

    Users : Migration & Model

--- app/User.php
+++ app/User.php
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the teacher that owns the slot.
+     */
+    public function report()
+    {
+        return $this->hasOne('App\Report', 'report_id');
+    }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,9 +16,13 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->date('date_of_birth');
+            $table->enum('gender', ['male', 'female', 'other']);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->enum('role', ['adm', 'prof', 'user'])->default('user');
+            $table->integer('report_id');
             $table->rememberToken();
             $table->timestamps();
         });
