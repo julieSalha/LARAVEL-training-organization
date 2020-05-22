edc54ab2e8084db15f9e5ea5a90167ede835c704
Guilhem Fouassier <guilhem.fouassier@gmail.com>
2020-05-22 18:21:26 +0200

    add relation training with teacher

--- app/Training.php
+++ app/Training.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Training extends Model
 {
-    //
+    /**
+     * Get the teacher that has the session.
+     */
+    public function teacher()
+    {
+        return $this->hasOne('App\User', 'teacher_id');
+    }
 }
