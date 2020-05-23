78b1d79781e967c71940f3298b482cd3eba94f3c
Julie Salha <jsalha@kaliop.com>
2020-05-23 16:58:13 +0200

    update relationships session model with training, room, teacher, report and grade

---
+++ app/Employee.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Teacher extends Model
+{
+    /**
+     * Get the teacher that has the session.
+     */
+//    public function user()
+//    {
+//        return $this->hasOne('App\User', 'id', 'teacher_id');
+ //   }
+}
