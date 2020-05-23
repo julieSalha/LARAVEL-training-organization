032036e14dfb86385d8ad695a7527b490928ce4c
Julie Salha <jsalha@kaliop.com>
2020-05-23 19:16:47 +0200

    update teacher-controller and his actions

--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -4,10 +4,10 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Les utilisateurs:</div>
+                <div class="card-header">Les utilisateurs :</div>
                 <a href="{{ route('add_user') }}">Ajoutez un utilisateur</a>
                 <div class="card-body">
-                    Tous les utilisateurs:
+                    Tous les utilisateurs :
                     <ul>
                     @foreach ($users as $user)
                         <li> {{$user->name}} ( {{$user->role}} ) - <a href="{{ route('edit_user', $user->id) }}">Edit</a> </li>
--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
 Route::get('/users', 'AdminController@index')->name('users');
 Route::get('/add_user', 'AdminController@add_user')->name('add_user');
 Route::post('/create_user', 'AdminController@create_user')->name('create_user');
@@ -33,3 +34,11 @@ Route::get('/edit_training/{id}', 'AdminController@edit_training')->name('edit_t
 Route::post('/update_training/{id}', 'AdminController@update_training')->name('update_training');
 Route::get('/delete_training/{id}', 'AdminController@delete_training')->name('delete_training');
 Route::get('/sessions/{id}', 'AdminController@sessions')->name('sessions');
+
+Route::get('/trainings', 'TeacherController@index')->name('trainings');
+Route::get('/sessions/{id}', 'TeacherController@sessions')->name('sessions');
+
+
+
+
+
